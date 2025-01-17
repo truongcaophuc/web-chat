11465f9ec40c29f37fe15871f8112eccf1bff1c5
Nguyen Que Phong <Ynwawithpogct@gmail.com>
2025-01-17 20:33:49 +0700

    update sections-dashboard - add settings

---
+++ Frontend/src/sections/dashboard/settings/ProfileForm.js
@@ -0,0 +1,106 @@
+import React, { useCallback, useState } from "react";
+import * as Yup from "yup";
+// form
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import FormProvider from "../../../components/hook-form/FormProvider";
+import { RHFTextField, RHFUploadAvatar } from "../../../components/hook-form";
+import { Stack } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { useDispatch, useSelector } from "react-redux";
+import { UpdateUserProfile } from "../../../redux/slices/app";
+
+
+const ProfileForm = () => {
+  const dispatch = useDispatch();
+  const [file, setFile] = useState();
+  const { user } = useSelector((state) => state.app);
+  const ProfileSchema = Yup.object().shape({
+    name: Yup.string().required("Name is required"),
+    about: Yup.string().required("About is required"),
+    avatar: Yup.string().required("Avatar is required").nullable(true),
+  });
+
+  const defaultValues = {
+    Name: user?.firstName+" "+user.lastName,
+    about: user?.about,
+    avatar: `https://www.kindpng.com/picc/m/421-4212275_transparent-default-avatar-png-avatar-img-png-download.png`,
+  };
+
+  const methods = useForm({
+    resolver: yupResolver(ProfileSchema),
+    defaultValues,
+  });
+
+  const {
+    reset,
+    watch,
+    control,
+    setValue,
+    handleSubmit,
+    formState: { isSubmitting, isSubmitSuccessful },
+  } = methods;
+
+  const values = watch();
+
+  const onSubmit = async (data) => {
+    try {
+      //   Send API request
+      console.log("DATA", data);
+      dispatch(
+        UpdateUserProfile({
+          Name: data?.name,
+          about: data?.about,
+          avatar: file,
+        })
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDrop = useCallback(
+    (acceptedFiles) => {
+      const file = acceptedFiles[0];
+
+      setFile(file);
+
+      const newFile = Object.assign(file, {
+        preview: URL.createObjectURL(file),
+      });
+
+      if (file) {
+        setValue("avatar", newFile, { shouldValidate: true });
+      }
+    },
+    [setValue]
+  );
+
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={4}>
+        <RHFUploadAvatar name="avatar" maxSize={3145728} onDrop={handleDrop} />
+
+        <RHFTextField
+          name="Name"
+          label="Name"
+        />
+        <RHFTextField multiline rows={4} name="about" label="About" />
+
+        <Stack direction={"row"} justifyContent="end">
+          <LoadingButton
+            color="primary"
+            size="large"
+            type="submit"
+            variant="contained"
+            // loading={isSubmitSuccessful || isSubmitting}
+          >
+            Save
+          </LoadingButton>
+        </Stack>
+      </Stack>
+    </FormProvider>
+  );
+};
+
+export default ProfileForm;
