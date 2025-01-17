1cb251fdc3185933d9cf085708cbcebe8e7d1c8d
Nguyen Que Phong <Ynwawithpogct@gmail.com>
2025-01-17 11:07:56 +0700

    add Profile_Menu

---
+++ Frontend/src/data/index.js
@@ -0,0 +1,29 @@
+import { faker } from "@faker-js/faker";
+import {
+  ChatCircleDots,
+  Gear,
+  GearSix,
+  Phone,
+  SignOut,
+  User,
+  Users,
+} from "phosphor-react";
+
+const Profile_Menu = [
+  {
+    title: "Profile",
+    icon: <User />,
+  },
+  {
+    title: "Settings",
+    icon: <Gear />,
+  },
+  {
+    title: "Sign Out",
+    icon: <SignOut />,
+  },
+];
+
+export {
+  Profile_Menu,
+};
